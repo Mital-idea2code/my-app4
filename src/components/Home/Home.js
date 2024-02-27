6064941c7da2a695edf3510cd788fdbc849dd188
Mital-idea2code <mk.idea2code@gmail.com>
2024-02-27 13:07:59 +0530

    HOmepage changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,16 +5,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./home.css"; // Create this file for custom styles
 import HeroBg from "../../assets/images/hero-bg.png";
 import shape1 from "../../assets/images/shape/1.svg";
+import shape2 from "../../assets/images/shape/2.svg";
 import shape3 from "../../assets/images/shape/3.svg";
 import shape4 from "../../assets/images/shape/4.svg";
-import { FaFacebook, FaLinkedin, FaTwitter, FaGoogle, FaInstagram } from "react-icons/fa";
+import shape5 from "../../assets/images/shape/33.jpg";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaGoogle,
+  FaInstagram,
+  FaUser,
+  FaEdit,
+  FaPen,
+  FaCertificate,
+  FaBook,
+  FaAddressBook,
+  FaPlusCircle,
+  FaUsers,
+  FaEnvelope,
+} from "react-icons/fa";
 
 const Home = () => {
   return (
     <Container fluid className="home-container">
       <Row>
         {/* Left side content */}
-        <Col xs={12} md={6} className="left-content  d-flex flex-column justify-content-center align-items-center">
+        <Col xs={12} md={7} className="left-content  d-flex flex-column justify-content-center align-items-center">
           <h1 className="headText">
             <span class="bg-ytext">HELLO! STRA</span>
             <span class="stran">NGER!</span>{" "}
@@ -90,22 +107,56 @@ const Home = () => {
             </ul>
           </div>
           <div>
-            <Button className="btn-color">Download CV</Button>
+            <Button className="btn-color download-btn">Download CV</Button>
           </div>
           {/* New shape with animation */}
+
           <Image src={shape1} alt="Shape1" fluid className="shape1" />
+          <Image src={shape2} alt="Shape2" fluid className="shape2" />
           <Image src={shape3} alt="Shape3" fluid className="shape3" />
           <Image src={shape4} alt="Shape4" fluid className="shape4" />
+          <Image src={shape3} alt="Shape3" fluid className="shape3-1" />
+          <Image src={shape5} alt="Shape5" fluid className="shape5" />
         </Col>
 
         {/* Center content */}
-        <Col xs={12} md={2} className="center-content d-flex flex-column justify-content-center align-items-center p-0">
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">ABOUT ME</Col>
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">EDUCATION</Col>
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">EXPERIENCE</Col>
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">PORTFOLIO</Col>
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">TESTIMONIAL</Col>
-          <Col className="home-box  d-flex flex-column justify-content-center align-items-center ">CONTACT ME</Col>
+        <Col xs={12} md={1} className="center-content d-flex flex-column justify-content-center align-items-center p-0">
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center about-bg">
+            <div class="background-container">
+              <FaUser className="icon" />
+              <span class="text">ABOUT ME</span>
+            </div>
+          </Col>
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center edu-bg ">
+            <div class="background-container">
+              <FaPen className="icon" />
+              <span class="text">EDUCATION</span>
+            </div>
+          </Col>
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center exeperience-bg ">
+            <div class="background-container">
+              <FaCertificate className="icon" />
+              <span class="text">EXPERIENCE</span>
+            </div>
+          </Col>
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center portfolio-bg ">
+            <div class="background-container">
+              <FaAddressBook className="icon" />
+              <span class="text">PORTFOLIO</span>
+            </div>
+          </Col>
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center testimonial-bg ">
+            <div class="background-container">
+              <FaUsers className="icon" />
+              <span class="text">TESTIMONIAL</span>
+            </div>
+          </Col>
+          <Col className="home-box  d-flex flex-column justify-content-center align-items-center contact-bg ">
+            <div class="background-container">
+              <FaEnvelope className="icon" />
+              <span class="text">CONTACT ME</span>
+            </div>
+          </Col>
         </Col>
 
         {/* Right side content */}
